72683fa6efa195056e797df6e2f5175ed13d650f
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2022-04-02 16:04:45 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,11 +5,18 @@ use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 
 $config = [
-    // Your driver-specific configuration
-    // "telegram" => [
-    //    "token" => "TOKEN"
-    // ]
 ];
+
+$servername = "";
+$username = "";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
 // Load the driver(s) you want to use
 DriverManager::loadDriver(\BotMan\Drivers\Telegram\TelegramDriver::class);
@@ -27,6 +34,30 @@ $botman->hears('I want ([0-9]+) (small coffee|medium coffee|large coffee|shot
     $cost = 5;
     $total = $number * $cost;
     $bot->reply('You have order'.$number.'cups of '.$coffee.'.')
+
+    /*
+      if(isset($_REQUEST["term"])){
+        $sql = "SELECT * FROM  drinks WHERE name LIKE ?";
+
+        if($stmt = mysqli_prepare($conn, $sql)){
+          mysqli_stmt_bind_param($stmt, "s", $param_term);
+          if(mysqli_num_rows($result) > 0){
+            echo "$row["price"]";
+          } else {
+            echo "Drink choice not availible";
+          }
+        } else {
+          echo "ERROR: Could not able to execute order"
+        }
+         mysqli_stmt_close($stmt);
+      }
+    */
+
+
+});
+
+$botman->hears('Good Bye', function (Botman $bot) {
+    $conn->close();
 });
 
 botman
